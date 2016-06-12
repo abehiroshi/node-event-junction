e3cac07c5f7e941eb4420507000b902e96facc24
Abe Hiroshi <abhrsh@gmail.com>
2016-06-12 13:16:27 +0000

    send event to url

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 import chokidar from 'chokidar'
+import rest from 'restler'
 
-console.log('test')
+const url = process.env.SEND_URL
+function sendEvent(event){
+  if (url) {
+    rest.postJson(url, event)
+    .on('error', (err, res)=>{
+      console.log('error')
+      console.dir(res)
+      console.dir(err);
+    })
+  } else {
+    console.dir(event)
+  }
+}
+
+function startWatch(pattern){
+  chokidar.watch(pattern, {ignored: /[\/\\]\./, persistent: true})
+    .on('error', err=>{
+      sendEvent({
+        name: 'watchFile',
+        status: 'error',
+        result: {error: err},
+      })
+    })
+    .on('all', (event, path)=>{
+      sendEvent({
+        name: 'watchFile',
+        status: event,
+        result: {path: path},
+      })
+    })
+}
+
+const pattern = process.env.WATCH_PATTERN
+if (pattern){
+  startWatch(pattern)
+} else {
+  console.error('環境変数 WATCH_PATTERN が設定されていません')
+}
